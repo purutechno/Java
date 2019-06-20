5161ed224454b399fb1bfcae63bddf045f704806
purutechno <44781006+purutechno@users.noreply.github.com>
2019-06-20 11:30:58 +0545

    Update Val.java

--- Val.java
+++ Val.java
@@ -1,66 +1,70 @@
+package matrixx;
+
 import java.util.Scanner;
 
-public class Val {
-	public static void main(String[]args) {
-	Scanner input = new Scanner(System.in);
-	System.out.println("Enter Rows of a Matrix : ");
-	int row = input.nextInt();
-	System.out.println("Enter Columns of a Matrix : ");
-	int col = input.nextInt();
-	int ele[][] = new int[row][col];
-	for (int a=0 ; a<row;a++) {
-		for (int b=0 ; b<col ;b++ ) {
-			System.out.printf("Enter elements of %d row and %d column : \n",a+1,b+1);
-			ele[a][b] = input.nextInt();  
-		}
-	}
-	for (int x=0;x<row;x++) {
-		for (int y=0;y<col;y++) {
-			System.out.printf(ele[x][y]+"\t");
-		}
-		System.out.printf("\n");
-	}
-	Scanner inpu = new Scanner(System.in);
-	System.out.println("Enter the rows : ");
-	int ro = inpu.nextInt();
-	System.out.println("Enter the columns : ");
-	int co = inpu.nextInt();
-	int mat[][] = new int[ro][co];
-	for (int e=0;e<ro;e++) {
-		for (int f=0;f<co;f++) {
-			System.out.printf("Enter the element of %d row and %d col \n ",e+1,f+1);
-			mat[e][f]  = inpu.nextInt();
-		}
-	}
-	for (int g=0;g<ro;g++) {
-		for (int h=0;h<co;h++) {
-			System.out.printf(mat[g][h]+"\t");
-		}
-		System.out.printf("\n");
-	}
-	int sum = 0;
-	int res[][] = new int[row][co];
-	if (col == ro) {
-		for(int i=0;i<row;i++) {
-			for (int j=0;j<co;j++) {
-				for (int k=0;k<col;k++) {
-					sum = sum + ele[i][k]*mat[k][j];
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("M1 : No of rows :");
+		int row = input.nextInt();
+		System.out.println("M1 : No of columns : ");
+		int col = input.nextInt();
+		System.out.println("M2 : No of rows :");
+		int row2 = input.nextInt();
+		System.out.println("M2 : No of columns : ");
+		int col2 = input.nextInt();
+		int[][] matrix1 = new int[row][col];
+		int[][] matrix2 = new int[row2][col2];
+		int[][] result = new int[row][col2];
+		if (col == row2) {
+			for(int a=0;a<row;a++) {
+				for(int b=0;b<col;b++) {
+					System.out.printf("M1 : Element of %d row %d column",(a+1),(b+1));
+					matrix1[a][b]= input.nextInt();
 				}
-				res[i][j] = sum;
-				sum = 0;
+			}
+			for (int a=0;a<row;a++) {
+				for (int b=0;b<col;b++){
+					System.out.printf(matrix1[a][b]+"\t");
+				}
+				System.out.printf("\n");
+			}
+			System.out.println("\n \n ");
+			for(int a=0;a<row2;a++) {
+				for(int b=0;b<col2;b++) {
+					System.out.printf("M2 : Element of %d row %d column",(a+1),(b+1));
+					matrix2[a][b]= input.nextInt();
+				}
+			}
+			System.out.println("\n" );
+			for(int a=0;a<row2;a++) {
+				for(int b=0;b<col2;b++) {
+					System.out.printf(matrix2[a][b]+"\t");
+				}
+				System.out.printf("\n");
+			}
+			System.out.println("M1 X M2 = ");
+			int sum = 0;
+			for (int a=0;a<row;a++ ) {
+				for (int b=0;b<col2;b++) {
+					for (int c=0;c<row2;c++) {
+							sum = sum + matrix1[a][c]*matrix2[c][b];					
+					}
+					result[a][b] = sum;
+					sum =0;
+				}
+			}
+			for(int a=0;a<row;a++) {
+				for(int b=0;b<col2;b++) {
+					System.out.printf(result[a][b]+"\t");
+				}
+				System.out.printf("\n");
 			}
 		}
-		System.out.println("\n"+"\n"+"\n");
-		for (int l=0;l<row;l++) {
-			for(int m=0;m<co;m++) {
-				System.out.printf(res[l][m]+"\t");
-			}
-			System.out.printf("\n");
+		else {
+			System.out.println("Matrix Cannot be multiplieds! ");
+		}
 	}
-	
-	}
-	else {
-		System.out.println("Sorry! The matrix cannot be multiplied !");
-	}
-	}
+
 }
